cb9e8fd1c541a35b205aaad6022a3ecf9d8da37a
Abhijith <abhijith.a@qburst.com>
2024-03-01 14:19:56 +0530

    Switch to Test

--- cypress/Tests/Basic.ts
+++ cypress/Tests/Basic.ts
@@ -21,3 +21,29 @@ it("learning assertions", function()
     let example: any;
 
 })
+
+it("learning assertions", function()
+{
+    let Array: number [] = [1,2,3,4,5];
+    let tuple: [number, number] = [1,2];
+
+    type point = {x: number, y: number}; //using type annotation to initialise the intent
+    let center: point = { x: 0, y: 2 }; //Javascript variables initialised to objects
+    center.x = 13; //We can directly modify the values of the objects using type variables.
+    center.y = 23;
+    let example: any;
+
+})
+
+it("learning assertions", function()
+{
+    let Array: number [] = [1,2,3,4,5];
+    let tuple: [number, number] = [1,2];
+
+    type point = {x: number, y: number}; //using type annotation to initialise the intent
+    let center: point = { x: 0, y: 2 }; //Javascript variables initialised to objects
+    center.x = 23; //We can directly modify the values of the objects using type variables.
+    center.y = 3;
+    let example: any;
+
+})
